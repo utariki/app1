c7a26e2c13858cff6666fd5446e06c6d01938c99
utariki <fordno123@gmail.com>
2023-11-10 11:48:23 +0700

    chang1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            พลวัต กุลนะ
+            พลวัต กุลนะ2
         </p>
         <a
           className="App-link"
